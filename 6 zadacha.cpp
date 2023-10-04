74185891365c4d59a27828ae5f295ed2bd9844ca
watermeloy <146944852+watermeloy@users.noreply.github.com>
2023-10-04 13:13:19 +0200

    Update and rename 6 zadacha.sln to 6 zadacha.cpp

---
+++ 6 zadacha.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+
+    const int maxSize = 100;
+    int sequence[maxSize];
+
+    int n;
+    cout << "Введите размер последовательности: ";
+    cin >> n;
+
+    cout << "Введите элементы последовательности: ";
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> sequence[i];
+    }
+
+    int maxNumber = 0;
+    int maxIndex = -1;
+    int product = 1;
+
+    for (int i = 0; i < n; i++) 
+    {
+        if (sequence[i] % 10 == 2) {
+            if (sequence[i] > maxNumber) {
+                maxNumber = sequence[i];
+                maxIndex = i;
+            }
+            product *= sequence[i];
+        }
+    }
+
+    cout << "Наибольшее число, оканчивающееся на 2: " << maxNumber << endl;
+    cout << "Номер этого числа в последовательности: " << maxIndex << endl;
+    cout << "Произведение чисел, оканчивающихся на 2: " << product << endl;
+
+    return 0;
+}
